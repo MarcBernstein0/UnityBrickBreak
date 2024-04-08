949c4e847a7f19f7d001436ca18b99d974d1625d
Marc Bernstein <marcbernstein98@gmail.com>
2024-04-07 20:02:02 -0400

    add ball and physics to the ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 200.0f;
+    public Vector2 start_pos = new Vector2(0.0f, -1.5f);
+    private Rigidbody2D _rigidBody;
+
+    void Awake() 
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void ResetPosition()
+    {
+        _rigidBody.position = start_pos;
+        _rigidBody.velocity = Vector2.zero;
+    }
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetPosition();
+        StartCoroutine(DelayStart(1));
+    }
+
+    public IEnumerator DelayStart(float seconds) 
+    {
+        yield return new WaitForSeconds(seconds);
+
+        AddStartingForce();
+    }
+
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? Random.Range(-0.5f, -1.0f) :
+                                        Random.Range(0.5f, 1.0f);
+        float y = -1.0f;
+        Vector2 direction = new Vector2(x, y);
+        _rigidBody.AddForce(direction * this.speed);
+    }
+
+}
